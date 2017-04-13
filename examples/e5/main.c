78c872f6afcf4d1c74f76a68b001255ed0e71d31
David Trabish <davidtr1037@gmail.com>
2017-04-13 21:32:09 +0300

    updating examples

--- examples/e5/main.c
+++ examples/e5/main.c
@@ -17,22 +17,23 @@
 typedef struct {
     dict_t dict;
     char *input;
-    unsigned int token_count;
     bool eof;
 } parser_t;
 
 void parser_init(parser_t *parser, char *input) {
     dict_init(&parser->dict, 10);
     parser->input = input;
-    parser->token_count = 0;
     parser->eof = false;
 }
 
-void parser_parse_name(parser_t *parser) {
+void parser_parse_token(parser_t *parser) {
     char *name = NULL;
     int len = 0;
 
-    while (*parser->input != 0 && *parser->input == '\n') {
+    while (*parser->input != 0) {
+        if (*parser->input != '\n') {
+            break;
+        }
         parser->input++;
     }
 
@@ -55,12 +56,18 @@ void parser_parse_name(parser_t *parser) {
         return;
     }
 
-    //dict_lookup(&parser->dict, name, len); 
+    dict_add(&parser->dict, name, len);
 }
 
+#define PARSE_TOKEN(parser) \
+    parser_parse_token(parser); \
+    if (parser->eof) { \
+        return; \
+    }
+
 void parser_parse_tokens(parser_t *parser) {
-    for (unsigned int i = 0; i < 5; i++) {
-        parser_parse_name(parser);
+    for (unsigned int i = 0; i < 4; i++) {
+        parser_parse_token(parser);
         if (parser->eof) {
             return;
         }
@@ -73,12 +80,13 @@ void parser_parse(parser_t *parser) {
         return;
     }
 
+    /* reachability test... */
     assert(false);
 }
 
 int main(int argc, char *argv[]) {
     parser_t parser;
-    char buf[10];
+    char buf[12];
 
     klee_make_symbolic(&buf, sizeof(buf), "buf");
     klee_assume(buf[sizeof(buf) - 1] == 0);
